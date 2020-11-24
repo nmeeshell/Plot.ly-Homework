a7d123692a326ef447c4795ac2b6d8ec34b87757
nmeeshell <65826269+nmeeshell@users.noreply.github.com>
2020-11-24 13:12:02 -0600

    update

--- static/js/app.js
+++ static/js/app.js
@@ -24,11 +24,14 @@ function getPlots(id) {
             console.log(sampleValues)
             var labels =  sampledata.samples[0].otu_labels.slice(0,10);
             console.log (labels)
+
         // get only top 10 otu ids for the plot OTU and reversing it. 
             var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+
         // get the otu id's to the desired form for the plot
             var OTU_id = OTU_top.map(d => "OTU " + d);
             console.log(`OTU IDS: ${OTU_id}`)
+            
          // get the top 10 labels for the plot
             var labels =  sampledata.samples[0].otu_labels.slice(0,10);
             console.log(`OTU_labels: ${labels}`)
